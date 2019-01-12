ba91629d30a88e996ae142b5d1f9e33edce71908
Brian Bird <ProfBird@users.noreply.github.com>
2019-01-11 22:16:36 -0800

    Resolved build issues on Windows

--- BookInfo/Startup.cs
+++ BookInfo/Startup.cs
@@ -47,8 +47,6 @@ namespace BookInfo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
